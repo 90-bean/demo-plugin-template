d81413bb8d5410ca934aaaa17dcc18006eab373a
GitHub Action <action@github.com>
2024-04-12 23:47:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/90bean/demoplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/90bean/demoplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.90bean.demoplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/90bean/demoplugintemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/90bean/demoplugintemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.90bean.demoplugintemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
